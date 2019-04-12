2486fe345fffdd7c05b6cde16ec642e5f2e637be
Tom Hsu <thomaschsu@gmail.com>
2019-04-12 12:23:11 -0400

    Modified some more js functionality

--- assets/app.js
+++ assets/app.js
@@ -1,29 +1,33 @@
 let colors = generateColors(6);
+let pickedColor = pickColor();
 
 const squares = document.querySelectorAll('.square');
-const pickedColor = pickColor();
 const message = document.querySelector('#message');
+const newColors = document.querySelector('#newColors');
 const jumbotron = document.querySelector('.jumbotron');
 
-for(let i = 0; i < squares.length; i++) {
-    // Add intial colors to squares
-    squares[i].style.backgroundColor = colors[i];
+function startGame() {
+    for (let i = 0; i < squares.length; i++) {
+        // Add intial colors to squares
+        squares[i].style.backgroundColor = colors[i];
 
-    // Add click listeners to squares
-    squares[i].addEventListener('click', function() {
-        // Grab color of clicked square
-        let clickedColor = this.style.backgroundColor;
-        console.log(clickedColor, pickedColor);
-        // Compare color to pickedColor
-        if(clickedColor === pickedColor) {
-            message.textContent = 'You win!';
-            changeColors(clickedColor);
-            jumbotron.style.background = clickedColor;
-        } else {
-            this.style.backgroundColor = '#FFF';
-            message.textContent = 'Try again!';
-        }
-    });
+        // Add click listeners to squares
+        squares[i].addEventListener('click', function () {
+            // Grab color of clicked square
+            let clickedColor = this.style.backgroundColor;
+            console.log(clickedColor, pickedColor);
+            // Compare color to pickedColor
+            if (clickedColor === pickedColor) {
+                message.textContent = 'You win!';
+                newColors.textContent = 'Play again?';
+                changeColors(clickedColor);
+                jumbotron.style.background = clickedColor;
+            } else {
+                this.style.backgroundColor = '#FFF';
+                message.textContent = 'Try again!';
+            }
+        });
+    }
 }
 
 const rgbValue = document.getElementById('rgbValue');
@@ -31,7 +35,7 @@ rgbValue.textContent = pickedColor;
 
 function changeColors(color) {
     // Loop through all squares
-    for(let i = 0; i < squares.length; i++) {
+    for (let i = 0; i < squares.length; i++) {
         // Change each color to match winning color
         squares[i].style.backgroundColor = color;
     }
@@ -46,7 +50,7 @@ function generateColors(num) {
     // Make Array
     let array = [];
     // Loop
-    for(let i = 0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
         // Make random colors and push into array
         array.push(randomColor());
     }
@@ -63,3 +67,20 @@ function randomColor() {
     let b = Math.floor(Math.random() * 256);
     return "rgb(" + r + ", " + g + ", " + b + ")";
 }
+
+newColors.addEventListener('click', function () {
+    // Generate new colors
+    colors = generateColors(6);
+    // Pick a new random color from array
+    pickedColor = pickColor();
+    // Change rgbValue to match picked color
+    rgbValue.textContent = pickedColor;
+    // Change colors of squares
+    startGame();
+    // Reset background of jumbotron
+    jumbotron.style.backgroundColor = "";
+    // Change text content back to new Colors
+    newColors.textContent = 'New Colors';
+});
+
+startGame();
